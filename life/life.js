236a4db7ccbf76d9e8f6afb0fb1a8f7d820ebede
danielbgd <ddanielbgd@gmail.com>
2016-08-27 07:53:31 +0200

    Added language selector and help tips

--- life/life.js
+++ life/life.js
@@ -11,20 +11,27 @@ var playPauseButton = imgFolder + "play-pause.png"; // lokacija simbola za "PLAY
 var stepButton = imgFolder + "step.png"; // lokacija simbola za "FRAME ADVANCE"
 var stepButtonDisabled = imgFolder + "step-disabled.png"; // lokacija deaktiviranog simbola za "FRAME ADVANCE"
 var mode = 0; // flag radnog režima (0 – STOP; 1 – PLAY; 2 – PAUSE)
+var fieldsArray = []; // niz u kojem se čuvaju statusi polja
+
+function init() {
+	english(); // prikazivanje natpisa i poruka na engleskom jeziku
+	drawTable(60,80,1); // formiranje tabele
+}
 
 function drawTable(content) { // crtanje nove tabele (prazne ili s predefinisanim sadržajem)
 	var i, j; // i – brojač vrsta; j – brojač kolona (vrste su numerisane od 1 do m, a kolone od 1 do n)
 	var a = ""; // tekstualna promenljiva u koju smeštamo HTML-kôd tekuće vrste tabele
 	counter = 0; // resetovanje brojača koraka, budući da je s prethodnom simulacijom završeno
 	document.getElementById("counter").innerHTML = ""; // uklanjanje prikaza broja koraka (iz istog razloga kao malopre)
-	if (ledLoop != 0) {
-		clearInterval(ledLoop);
+	if (ledLoop != 0) { // ako LED-dioda treperi...
+		clearInterval(ledLoop); // ...zaustavi treperenje
 	}
-	if (mode == 2) {
-		document.getElementById("start").getElementsByTagName("img")[0].src = playButton;
-		mode = 0;
+	if (mode == 2) { // ako je režim bio „PAUSE“...
+		mode = 0; // ...prelazak u režim „STOP“
+		document.getElementById("start").getElementsByTagName("img")[0].src = playButton; // na aktivacionom dugmetu prikazuje se oznaka „PLAY“
+		document.getElementById("start-tip").innerHTML = langArray["start tip"]; // help tip se postavlja na „pokretanje simulacije“
 		ledLoop = 0;
-		document.getElementById("led").className = "led-off";
+		document.getElementById("led").className = "led-off"; // LED se isključuje
 	}
 	for (i = 1; i <= m; i++) { // petlja po vrstama
 		a += "<tr>";
@@ -100,7 +107,12 @@ function changeField(x) { // promena statusa polja pod rednim brojem x – ako j
 
 function inverse() { // zamena svih živih polja mrtvima i obratno
 	for (i = 1; i <= p; i++) { // petlja po poljima tabele (polja ima p=m*n)
-		changeField(i);
+		changeField(i); // promena statusa tekućeg polja tabele
+		fieldsArray[i] = 1 - fieldsArray[i] // promena vrednosti tekućeg člana niza
+	}
+	if (document.getElementById("counter").innerHTML != "") { // ukoliko vrednost brojača nije nula, tj. ako je simulacija već započeta...
+		counter = 0; // resetovanje brojača...
+		dispCounter(); // ...i njegovo ponovno prikazivanje
 	}
 }
 
@@ -120,7 +132,6 @@ function newStatus(x,count) { // vrednost funkcije predstavlja novi status ćeli
 }
 
 function step() { // korak simulacije
-	var fieldsArray = new Array; // kreiramo privremeni niz u kojem ćemo čuvati nove statuse polja
 	var count; // varijabla koja pokazuje koliko tekuće polje ima živih suseda
 
 	for (i = 1; i <= p; i++) { // inicijalno ispunjavanje privremenog niza nulama (niz ima p=m*n članova, tj. onoliko koliko ima polja tabele)
@@ -181,10 +192,13 @@ function step() { // korak simulacije
 		}
 	}
 
-	// prikazivanje tekućeg koraka
+	// prikazivanje rednog broja koraka simulacije
 	counter++;
-	document.getElementById("counter").innerHTML = "Redni broj koraka: <span class='counter'>" + counter + "</span>";
+	dispCounter();
+}
 
+function dispCounter() { // prikazivanje rednog broja koraka simulacije (na osnovu sadržaja globalne varijable counter)
+	document.getElementById("counter").innerHTML = langArray["step count"] + " <span class='counter'>" + counter + "</span>";
 }
 
 function setSpeed() { // podešavanje brzine na osnovu položaja klizača brzine i ponavljanje radne petlje
@@ -195,10 +209,12 @@ function setSpeed() { // podešavanje brzine na osnovu položaja klizača brzine
 function run() { // Prelazak u režim „PLAY“ ili „PAUSE“
 	if (mode == 1) { // ako je režim bio „PLAY“, prelazak u režim „PAUSE“
 		clearInterval(runLoop); // zaustavlja se radna petlja
-		document.getElementById("start").getElementsByTagName("img")[0].src = playPauseButton; // na aktivacionom dugmetu se prikazuju simboli "PLAY" i "PAUSE"
 		mode = 2; // flag radnog režima se postavlja na vrednost koji označava režim „PAUSE“
-		document.getElementById("step").disabled = false; // aktivira se dugme „Jedan korak“ (koje je bilo deaktivirano tokom režima „PLAY“)
-		document.getElementById("step").getElementsByTagName("img")[0].src = stepButton; // u režimu „PAUSE“ dugme „Jedan korak“ je aktivirano
+		document.getElementById("start").getElementsByTagName("img")[0].src = playPauseButton; // na aktivacionom dugmetu se prikazuju simboli "PLAY" i "PAUSE"
+		document.getElementById("start-tip").innerHTML = langArray["cont tip"]; // help tip se postavlja na „nastavljanje simulacije“
+		document.getElementById("step").disabled = false; // aktivira se dugme „FRAME ADVANCE“ (koje je bilo deaktivirano tokom režima „PLAY“)
+		document.getElementById("step").getElementsByTagName("img")[0].src = stepButton; // aktivira se i slika njegovog simbola na tasteru
+		document.getElementById("step-tip").className = "tip"; // samim tim, pošto je dugme aktivirano, treba da se hoverom na njega ispisuje i help tip
 		document.getElementById("led").className = "led-off"; // LED se isključuje pre započinjanja treperućeg režima
 		ledFlag = 0; // pre ulaska u petlju za treperenje postavljamo indikator da je LED isključena
 		ledLoop = setInterval(ledBlink, 500); // petlja za treperenje LED-diode, s periodom od 500ms
@@ -208,11 +224,13 @@ function run() { // Prelazak u režim „PLAY“ ili „PAUSE“
 			clearInterval(ledLoop); // ...zaustavlja se treperenje diode...
 			ledLoop = 0; // ...i varijabla timing-eventa treperenja se postavlja na nulu
 		}
-		document.getElementById("start").getElementsByTagName("img")[0].src = pauseButton; // na aktivacionom dugmetu se prikazuje simbol "PAUSE"
 		mode = 1; // flag radnog režima se postavlja na vrednost koji označava režim „PLAY“
+		document.getElementById("start").getElementsByTagName("img")[0].src = pauseButton; // na aktivacionom dugmetu se prikazuje simbol "PAUSE"
+		document.getElementById("start-tip").innerHTML = langArray["pause tip"]; // help tip se postavlja na „pauziranje simulacije“
 		document.getElementById("led").className = "led-on"; // LED se uključuje
-		document.getElementById("step").disabled = true; // za vreme „PLAY“ režima, dugme „Jedan korak“ je deaktivirano
-		document.getElementById("step").getElementsByTagName("img")[0].src = stepButtonDisabled; // u režimu „PLAY“ dugme „Jedan korak“ je deaktivirano
+		document.getElementById("step").disabled = true; // za vreme „PLAY“ režima, dugme „FRAME ADVANCE“ je deaktivirano
+		document.getElementById("step").getElementsByTagName("img")[0].src = stepButtonDisabled; // deaktivira se i slika njegovog simbola na tasteru
+		document.getElementById("step-tip").className = "tip-deact"; // samim tim, pošto je dugme deaktivirano, hoverom na njega ne treba da se ispisuje help tip
 		setSpeed(); // podešavanje brzine na osnovu položaja klizača brzine i ponavljanje radne petlje
 	}
 }
@@ -232,6 +250,129 @@ function changeSpeed () { // promena brzine u toku simulacije
 		clearInterval(runLoop); // prekida se dosadašnja radna petlja...
 		setSpeed(); // ...i startuje nova, s novopodešenom brzinom
 	}
+}
+
+// ispunjavanje niza natpisima i porukama na engleskom jeziku
+function english() {
+	langArray = []; // niz u kojem se čuvaju natpisi i poruke na engleskom jeziku
+	
+	// naslov
+	langArray["title"] = "The Game of Life";
+	
+	// natpisi
+	langArray["step count"] = "Step count:";
+	langArray["rand"] = "Random layout:";
+	langArray["thin"] = "thin";
+	langArray["thick"] = "thick";
+	langArray["all whites"] = "All whites";
+	langArray["all blacks"] = "All blacks";
+	langArray["hbars"] = "Horizontal bars";
+	langArray["vbars"] = "Vertical bars";
+	langArray["dbars"] = "Diagonal bars";
+	langArray["chess"] = "Chess layout";
+	langArray["inverse"] = "Inverse layout";
+	langArray["slow"] = "slow";
+	langArray["fast"] = "fast";
+	
+	// poruke
+	langArray["random tip"] = "Regulator for the black (alive) fields density<br />in the initial step.<ul><li>Leftmost position – minimal density;</li><li>Rightmost position – maximal density;</li></ul>Keyboard shortcuts: <span class='keyshort'>1–9</span><ul><li>key 1 – minimal density</li><li>key 9 – maximal density</li></ul>";
+	langArray["white tip"] = "Populating the table<br />with all the white (dead) fields.<br />Keyboard shortcut: <span class='keyshort'>A</span>";
+	langArray["black tip"] = "Populating the table<br />with all the black (alive) fields.<br />Keyboard shortcut: <span class='keyshort'>S</span>";
+	langArray["hor tip"] = "The arrangement of the alive<br />and the dead fields<br />in the form of horizontal bars<br />(every odd row – alive fields;<br />every even row – dead fields).<br />Keyboard shortcut: <span class='keyshort'>D</span>";
+	langArray["vert tip"] = "The arrangement of the alive<br />and the dead fields<br />in the form of vertical bars<br />(every odd column – alive fields;<br />every even column – dead fields).<br />Keyboard shortcut: <span class='keyshort'>F</span>";
+	langArray["diag tip"] = "The arrangement of the alive<br />and the dead fields<br />in the form of diagonal bars.<br />Keyboard shortcut: <span class='keyshort'>G</span>";
+	langArray["chess tip"] = "The arrangement of the alive<br />and the dead fields<br />in the form of a chessboard fields.<br />Keyboard shortcut: <span class='keyshort'>H</span>";
+	langArray["inverse tip"] = "Replaces all the black (live) fields<br />with the white (dead) fields and vice versa;<br />after this, the simulation step counter<br />returns to zero, since hereby<br />the new simulation is started.<br />Keyboard shortcut: <span class='keyshort'>I</span>";
+	langArray["step tip"] = "Executes one<br />simulation step.<br />Keyboard shortcut: <span class='keyshort'>X</span>";
+	langArray["start tip"] = "Starts the simulation.<br />Keyboard shortcut: <span class='keyshort'>C</span>";
+	langArray["pause tip"] = "Pauses the simulation.<br />Keyboard shortcut: <span class='keyshort'>C</span>";
+	langArray["cont tip"] = "Continues the simulation.<br />Keyboard shortcut: <span class='keyshort'>C</span>";
+	langArray["led tip"] = 'Mode LED-indicator:<ul><li>off: "STOP" mode</li><li>on: "PLAY" mode</li><li>blinking: "PAUSE" mode</li></ul>';
+	langArray["speed tip"] = "Simulation speed regulator:<ul><li>Leftmost position – minimal speed</li><li>Rightmost position – maximal speed</li></ul>Keyboard shortcuts:<br /><ul><li><span class='keyshort'>N</span> (reduces speed)</li><li><span class='keyshort'>M</span> (increase speed)</li></ul>";
+	document.getElementById("eng").className = "selected"; // simbol izabranog jezika treba da bude uokviren i da na njemu kursor miša bude default
+	document.getElementById("srb").className = ""; // simbol neizabranog jezika treba da bude neuokviren i da na njemu kursor miša bude pointer
+	writeLang(langArray); // pozivanje funkcija za prikaz natpisa i poruka
+}
+
+// ispunjavanje niza natpisima i porukama na srpskom jeziku
+function serbian() {
+	langArray = []; // niz u kojem se čuvaju natpisi i poruke na srpskom jeziku
+	
+	// naslov
+	langArray["title"] = "Igra života";
+	
+	// natpisi
+	langArray["step count"] = "Redni broj koraka:";
+	langArray["rand"] = "Random raspored:";
+	langArray["thin"] = "retko";
+	langArray["thick"] = "gusto";
+	langArray["all whites"] = "Sva bela";
+	langArray["all blacks"] = "Sva crna";
+	langArray["hbars"] = "Horizontalne pruge";
+	langArray["vbars"] = "Vertikalne pruge";
+	langArray["dbars"] = "Dijagonalne pruge";
+	langArray["chess"] = "Šahovski raspored";
+	langArray["inverse"] = "Inverzan raspored";
+	langArray["slow"] = "sporo";
+	langArray["fast"] = "brzo";
+	
+	// poruke
+	langArray["random tip"] = "Regulator gustine crnih (živih) polja<br />u početnom koraku.<ul><li>Krajnji levi položaj – najmanja gustina;</li><li>Krajnji desni položaj – najveća gustina;</li></ul>Prečice na tastaturi: <span class='keyshort'>1–9</span><ul><li>taster 1 – najmanja gustina</li><li>taster 9 – najveća gustina</li></ul>";
+	langArray["white tip"] = "Popunjavanje tabele<br />svim belim (neživim) poljima.<br />Prečica na tastaturi: <span class='keyshort'>A</span>";
+	langArray["black tip"] = "Popunjavanje tabele<br />svim crnim (živim) poljima.<br />Prečica na tastaturi: <span class='keyshort'>S</span>";
+	langArray["hor tip"] = "Raspoređivanje živih i mrtvih polja<br />u vidu horizontalnih pruga<br />(svaki neparni red – živa polja;<br />svaki parni red – mrtva polja).<br />Prečica na tastaturi: <span class='keyshort'>D</span>";
+	langArray["vert tip"] = "Raspoređivanje živih i mrtvih polja<br />u vidu vertikalnih pruga<br />(svaka neparna kolona – živa polja;<br />svaka parna kolona – mrtva polja).<br />Prečica na tastaturi: <span class='keyshort'>F</span>";
+	langArray["diag tip"] = "Raspoređivanje živih i mrtvih polja<br />u vidu dijagonalnih pruga.<br />Prečica na tastaturi: <span class='keyshort'>G</span>";
+	langArray["chess tip"] = "Raspoređivanje živih i mrtvih polja<br />u vidu polja na šahovskoj tabli.<br />Prečica na tastaturi: <span class='keyshort'>H</span>";
+	langArray["inverse tip"] = "Zamena svih crnih (živih) polja<br />belim (mrtvim) poljima i obratno;<br />nakon ovoga, brojač koraka<br />simulacije se vraća na nulu,<br />budući da ovime počinje<br />nova simulacija.<br />Prečica na tastaturi: <span class='keyshort'>I</span>";
+	langArray["step tip"] = "Izvršavanje jednog<br />koraka simulacije.<br />Prečica na tastaturi: <span class='keyshort'>X</span>";
+	langArray["start tip"] = "Pokretanje simulacije.<br />Prečica na tastaturi: <span class='keyshort'>C</span>";
+	langArray["pause tip"] = "Pauziranje simulacije.<br />Prečica na tastaturi: <span class='keyshort'>C</span>";
+	langArray["cont tip"] = "Nastavljanje simulacije.<br />Prečica na tastaturi: <span class='keyshort'>C</span>";
+	langArray["led tip"] = "Indikator režima rada:<ul><li>isključeno: režim „STOP“</li><li>uključeno: režim „PLAY“</li><li>treperenje: režim „PAUSE“</li></ul>";
+	langArray["speed tip"] = "Regulator brzine simulacije:<ul><li>Krajnji levi položaj – najmanja brzina</li><li>Krajnji desni položaj – najveća brzina</li></ul>Prečice na tastaturi:<br /><ul><li><span class='keyshort'>N</span> (smanjivanje brzine)</li><li><span class='keyshort'>M</span> (povećavanje brzine)</li></ul>";
+	document.getElementById("srb").className = "selected"; // simbol izabranog jezika treba da bude uokviren i da na njemu kursor miša bude default
+	document.getElementById("eng").className = ""; // simbol neizabranog jezika treba da bude neuokviren i da na njemu kursor miša bude pointer
+	writeLang(langArray); // pozivanje funkcija za prikaz natpisa i poruka
+}
+
+function writeLang() { // prikazivanje natpisa i poruka na izabranom jeziku, na osnovu sadržaja niza langArray[]
+	document.getElementsByTagName("title")[0].innerHTML = langArray["title"];
+	document.getElementsByTagName("h1")[0].innerHTML = langArray["title"];
+	document.getElementById("rand").innerHTML = langArray["rand"];
+	document.getElementById("rand-thin").innerHTML = langArray["thin"];
+	document.getElementById("rand-thick").innerHTML = langArray["thick"];
+	document.getElementById("white").innerHTML = langArray["all whites"];
+	document.getElementById("black").innerHTML = langArray["all blacks"];
+	document.getElementById("hor").innerHTML = langArray["hbars"];
+	document.getElementById("vert").innerHTML = langArray["vbars"];
+	document.getElementById("diag").innerHTML = langArray["dbars"];
+	document.getElementById("chess").innerHTML = langArray["chess"];
+	document.getElementById("inverse").innerHTML = langArray["inverse"];
+	document.getElementById("slow").innerHTML = langArray["slow"];
+	document.getElementById("fast").innerHTML = langArray["fast"];
+	document.getElementById("random-tip").innerHTML = langArray["random tip"];
+	document.getElementById("white-tip").innerHTML = langArray["white tip"];
+	document.getElementById("black-tip").innerHTML = langArray["black tip"];
+	document.getElementById("hor-tip").innerHTML = langArray["hor tip"];
+	document.getElementById("vert-tip").innerHTML = langArray["vert tip"];
+	document.getElementById("diag-tip").innerHTML = langArray["diag tip"];
+	document.getElementById("chess-tip").innerHTML = langArray["chess tip"];
+	document.getElementById("inverse-tip").innerHTML = langArray["inverse tip"];
+	document.getElementById("step-tip").innerHTML = langArray["step tip"];
+	switch(mode) { // help tip aktivacionog dugmeta se razlikuje u zavisnosti od toga da li je trenutni radni režim „STOP“, „PLAY“ ili „PAUSE“
+		case 0: // ukoliko je trenutni radni režim „STOP“...
+			document.getElementById("start-tip").innerHTML = langArray["start tip"]; // ...prikazuje se poruka „pokretanje simulacije“
+			break;
+		case 1: // ukoliko je trenutni radni režim „PLAY“...
+			document.getElementById("start-tip").innerHTML = langArray["pause tip"]; // ...prikazuje se poruka „paziranje simulacije“
+			break;
+		case 2: // ukoliko je trenutni radni režim „PAUSE“...
+			document.getElementById("start-tip").innerHTML = langArray["cont tip"]; // ...prikazuje se poruka „nastavljanje simulacije“
+			break;
+	}
+	document.getElementById("led-tip").innerHTML = langArray["led tip"];
+	document.getElementById("speed-tip").innerHTML = langArray["speed tip"];
 }
 
 document.onkeypress = key;
